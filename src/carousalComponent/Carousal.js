1ed91428294ccab053ecf638436552182680c62c
AnilKumarJadi <156993419+AnilKumarJadi@users.noreply.github.com>
2024-03-15 12:38:10 +0530

    Update Carousal.js

--- src/carousalComponent/Carousal.js
+++ src/carousalComponent/Carousal.js
@@ -6,13 +6,13 @@ const Carousal = () => {
       <div id="carouselExampleInterval" className="carousel slide" data-bs-ride="carousel">
   <div className="carousel-inner">
     <div className="carousel-item active" data-bs-interval="10000">
-      <img src="image/1.webp" className="d-block w-100" alt="..."/>
+      <img src="/image/1.webp" className="d-block w-100" alt="..."/>
     </div>
     <div className="carousel-item" data-bs-interval="2000">
-      <img src="image/2.webp" className="d-block w-100" alt="..."/>
+      <img src="./image/2.webp" className="d-block w-100" alt="..."/>
     </div>
     <div className="carousel-item">
-      <img src="image/3.webp" className="d-block w-100" alt="..."/>
+      <img src="../image/3.webp" className="d-block w-100" alt="..."/>
     </div>
   </div>
   <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
