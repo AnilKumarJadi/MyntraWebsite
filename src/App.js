4353abf4af08a00757cc9ff636e7139db6eda341
AnilKumarJadi <156993419+AnilKumarJadi@users.noreply.github.com>
2024-03-15 14:51:14 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import NavBar from "./navbar/Navbar";
 import Routee from "./routing/Routee";
 import {Suspense} from 'react';
+import Carousal from "./carousalComponent/Carousal";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Suspense fallback={<h1>Loading............!</h1>}>
 
       <NavBar />
+    <Carousal/>
       <Routee />
       </Suspense>
 
